13b8ddb6478430e355a7ef6a5e7db4fa6d00b685
Tom Evanko <tevanko@outlook.com>
2025-04-30 14:06:50 -0400

    Got ride of an unused import throwing an error

--- tomsportfolio/src/pages/TestPage.jsx
+++ tomsportfolio/src/pages/TestPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { config } from '../config';
 
 const TestPage = () => {
     const [results, setResults] = useState({
